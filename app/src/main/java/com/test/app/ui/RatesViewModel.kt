46e0920684bb8e3909bbf647dad5cbf916c885ee
Tan Tran Minh <tan.tran@grabtaxi.com>
2019-12-22 09:16:50 +0800

    Add icon resources

--- app/src/main/java/com/test/app/ui/RatesViewModel.kt
+++ app/src/main/java/com/test/app/ui/RatesViewModel.kt
@@ -1,12 +1,28 @@
 package com.test.app.ui
 
+import android.content.Context
+import androidx.annotation.DrawableRes
 import com.test.app.model.CurrencyRateResponse
 import com.test.app.service.CurrencyRateRepository
 import io.reactivex.Single
+import java.util.Currency
+import java.util.Locale
+
+private const val flagIconPrefix = "ic_flag_"
 
 class RatesViewModel(private val repository: CurrencyRateRepository) {
 
     fun getCurrencyRates(base: String): Single<CurrencyRateResponse> {
         return repository.getCurrencyRate(base)
+    }
+
+    fun getDisplayName(currencyCode: String): String {
+        return Currency.getInstance(currencyCode).displayName
+    }
+
+    @DrawableRes
+    fun getDrawableResId(context: Context, currencyCode: String): Int {
+        val resIcon = flagIconPrefix + currencyCode.toLowerCase(Locale.getDefault())
+        return context.resources.getIdentifier(resIcon, "drawable", context.packageName)
     }
 }
