04b091698edc4c1acaa08ddbc440fad6da6b3a7c
Tan Tran Minh <tan.tran@grabtaxi.com>
2019-12-27 10:23:19 +0800

    Add HttpLoggingInterceptor

--- app/src/main/java/com/test/app/service/RetrofitProvider.kt
+++ app/src/main/java/com/test/app/service/RetrofitProvider.kt
@@ -1,12 +1,18 @@
 package com.test.app.service
 
 import com.squareup.moshi.Moshi
+import com.test.app.BuildConfig
 import io.reactivex.Scheduler
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 private const val HOST: String = "https://revolut.duckdns.org/"
+private const val TIMEOUT = 30L
 
 class RetrofitProvider(
     private val moshi: Moshi,
@@ -18,6 +24,23 @@ class RetrofitProvider(
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(schedulers))
             .baseUrl(HOST)
+            .client(getOkHttpClient())
             .build()
+    }
+
+    private fun getOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(getLoggingInterceptor())
+            .build()
+    }
+
+    private fun getLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { Timber.d(it) }).setLevel(
+            if (BuildConfig.DEBUG)
+                HttpLoggingInterceptor.Level.BODY
+            else
+                HttpLoggingInterceptor.Level.NONE
+        )
     }
 }
